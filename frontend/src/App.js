4d7b6bbc8fa21651d78a2a962eddf215b572c1bb
Emilie <emilie.ost1990@gmail.com>
2022-02-20 11:15:43 +0100

    init project

---
+++ frontend/src/App.js
@@ -0,0 +1,5 @@
+import {createApp} from 'vue'
+
+import App from './App'
+
+export const app = createApp(App)
---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app">
+
+    </div>
+</template>
+
+<script>
+import {app} from './App'
+
+export default {
+    name: 'App'
+}
+app.mount('#app')
+</script>
+
+<style scoped>
+
+</style>
